07225e7469accc4f3e75825ab2dbcfc283605675
Smefinka <smefinka1@gmail.com>
2024-03-04 17:53:45 +0100

    edit incons in navigation + edit logo icon

--- src/app/components/Navigation/navigation.tsx
+++ src/app/components/Navigation/navigation.tsx
@@ -1,17 +1,30 @@
+import CategoryIcon from '../category-icon';
+import { Category } from '../../components/category-icon'
 import { BirdIcon, CatIcon, DogIcon, FishIcon } from '../icons';
 import styles from './Navigation.module.css'; 
+
+
 function Navigation(){
-  const ulClasses = `flex fontFamily-sans md:h-20 items-center ${styles.navText} bg-cyan-light md:rounded-xl overflow-x-auto`;
+  const ulClasses = `flex fontFamily-sans md:h-20 min-h-12 items-center ${styles.navText} bg-cyan-light md:rounded-xl overflow-x-auto`;
   const navClasses = `md:container mx-auto bg-cyan ${styles.scrollableContainer}`
    return(
        <>
        <nav className={navClasses}>
      <ul className={ulClasses}>
-       <li className="md:flex-1 text-center md:mx-4 px-3 shrink-0"> <DogIcon className="inline" />
-Собаки </li>
-       <li className="md:flex-1 text-center   mx-4 shrink-0"> <CatIcon className="inline" /> Коти</li>
-       <li className="md:flex-1 text-center  mx-4 shrink-0"> <BirdIcon className="inline"/> Птахи</li>
-       <li className="md:flex-1 text-center  mx-4 shrink-0"><FishIcon  className="inline"/> Риби</li>
+       <li className="md:flex-1 text-center mx-2 px-3 shrink-0">
+      <a href="#"><CategoryIcon category={Category.Dog}  /> <span>Собаки</span></a> 
+         {/* <DogIcon className="inline" /> */}
+ </li>
+       <li className="md:flex-1 text-center px-3  mx-2 shrink-0">
+       <CategoryIcon category={Category.Cat}  /> <span>Коти</span> </li>
+       <li className="md:flex-1 text-center px-3  mx-2 shrink-0"> 
+       {/* <BirdIcon className="inline"/> Птахи */}
+       <CategoryIcon category={Category.Bird}  /> <span>Птахи</span>
+       </li>
+       <li className="md:flex-1 text-center px-3  mx-2 shrink-0">
+        {/* <FishIcon  className="inline"/> Риби */}
+        <CategoryIcon category={Category.Fish}  /> <span>Риби</span>
+        </li>
      </ul>
        </nav>
         </>
--- src/app/components/category-icon.tsx
+++ src/app/components/category-icon.tsx
@@ -25,51 +25,51 @@ function CategoryIcon({
   switch (category) {
     case 'dog':
       return (
-        <div {...rest}>
+    
           <DogIcon
             className={clsx(
-              'w-[19.72px] h-[19.34px] xl:w-[52px] xl:h-[51px] fill-white hover:fill-yellow',
+              'w-[19.72px] h-[19.34px] xl:w-[52px] xl:h-[51px] fill-white hover:fill-yellow inline',
               active && ' fill-orange',
               disabled && 'cursor-not-allowed',
             )}
           />
-        </div>
+ 
       );
     case 'cat':
       return (
-        <div {...rest}>
+    
           <CatIcon
             className={clsx(
-              'w-[22px] h-[21.63px] xl:w-[51px] xl:h-[51px] fill-white hover:fill-yellow',
+              'w-[22px] h-[21.63px] xl:w-[51px] xl:h-[51px] fill-white hover:fill-yellow inline',
               active && ' fill-orange',
               disabled && 'cursor-not-allowed',
             )}
           />
-        </div>
+
       );
     case 'bird':
       return (
-        <div {...rest}>
+     
           <BirdIcon
             className={clsx(
-              'w-[12px] h-[22px] xl:w-[26px] xl:h-[51px] fill-white hover:fill-yellow',
+              'w-[12px] h-[22px] xl:w-[26px] xl:h-[51px] fill-white hover:fill-yellow inline',
               active && ' fill-orange',
               disabled && 'cursor-not-allowed',
             )}
           />
-        </div>
+      
       );
     case 'fish':
       return (
-        <div {...rest}>
+
           <FishIcon
             className={clsx(
-              'w-[19px] h-[22px] xl:w-[44px] xl:h-[51px] fill-white hover:fill-yellow',
+              'w-[19px] h-[22px] xl:w-[44px] xl:h-[51px] fill-white hover:fill-yellow inline',
               active && ' fill-orange',
               disabled && 'cursor-not-allowed',
             )}
           />
-        </div>
+   
       );
     default:
       return <svg></svg>;
