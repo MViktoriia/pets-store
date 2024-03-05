370206d304cadef189fad08382b8490e31fb084e
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-03-05 16:34:23 +0200

    used swiper in section brands

--- src/app/components/brands.tsx
+++ src/app/components/brands.tsx
@@ -1,8 +1,12 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
-import partner1 from '../../../public/images/partner-1-logo-2x.png';
-import partner2 from '../../../public/images/partner-2-logo-2x.png';
-import partner3 from '../../../public/images/partner-3-logo-2x.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import { brands } from '../lib/utils/constants/brands';
 
 function Brends() {
   return (
@@ -11,23 +15,41 @@ function Brends() {
         <h2 className=" font-serif text-[24px] text-center font-normal mb-6 lg:text-[36px] lg:mb-[57px]">
           Бренди з якими ми працюємо
         </h2>
-        <ul className="flex justify-center items-center">
-          <li className=" mr-[35px] last:mr-0 xl:mr-[144px]">
-            <div className="w-[24vw] xl:w-[200px]">
-              <Image src={partner1} alt="partner logo" />
-            </div>
-          </li>
-          <li className=" mr-[35px] last:mr-0 xl:mr-[144px]">
-            <div className="w-[24vw] xl:w-[200px]">
-              <Image src={partner2} alt="partner logo" />
-            </div>
-          </li>
-          <li className=" mr-[35px] last:mr-0 xl:mr-[144px]">
-            <div className="w-[24vw] xl:w-[200px]">
-              <Image src={partner3} alt="partner logo" />
-            </div>
-          </li>
-        </ul>
+        <Swiper
+          wrapperTag="ul"
+          wrapperClass="items-center"
+          spaceBetween={35}
+          slidesPerView={3}
+          autoplay={{
+            delay: 2500,
+          }}
+          loop
+          modules={[Autoplay]}
+          breakpoints={{
+            1024: {
+              slidesPerView: 4,
+            },
+            1440: {
+              slidesPerView: 4,
+              spaceBetween: 144,
+            },
+          }}
+        >
+          {brands.map((brand) => (
+            <SwiperSlide
+              key={brand.id}
+              tag="li"
+              className="w-[91px] xl:w-[200px] flex justify-center items-center"
+            >
+              <Image
+                width={200}
+                height={200}
+                src={brand.logo}
+                alt={`${brand.name} logo`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
---
+++ src/app/lib/utils/constants/brands.ts
@@ -0,0 +1,8 @@
+//for section brends on main page
+export const brands = [
+  { id: '1', name: 'Hills', logo: '/images/partner-1-logo-2x.png' },
+  { id: '2', name: 'Pedigree', logo: '/images/partner-2-logo-2x.png' },
+  { id: '3', name: 'Brit care', logo: '/images/partner-3-logo-2x.png' },
+  { id: '4', name: 'Royal Canin', logo: '/images/partner-4-logo-2x.png' },
+  { id: '5', name: 'Brit care', logo: '/images/partner-3-logo-2x.png' },
+];
