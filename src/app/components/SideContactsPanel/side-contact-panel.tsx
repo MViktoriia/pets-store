e07d2f53095a78dfa30a1fc1045a4a76b396daaa
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-04-07 15:08:44 +0300

    added hover and active on socials, some bugfix of phone number hover

--- src/app/components/SideContactsPanel/side-contact-panel.tsx
+++ src/app/components/SideContactsPanel/side-contact-panel.tsx
@@ -4,7 +4,7 @@ import styles from './side-contact-panel.module.css';
 
 function SideContactPanel() {
   return (
-    <div className="hidden md:block fixed z-20 right-0 top-[352px] w-[90px] bg-yellow rounded-l-xl px-5 py-6">
+    <div className="hidden md:block fixed z-20 right-0 top-[352px] w-[90px] bg-yellow rounded-l-xl py-6">
       <Socials className=" flex-col" />
       <Phone isFilled className={styles.phoneNumber} />
     </div>
