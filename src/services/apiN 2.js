29915376bbfc2b9a070a190bedb32271776788ab
Smefinka <smefinka1@gmail.com>
2024-06-12 10:13:50 +0200

    Delete src/services/apiN 2.js

--- src/services/apiN 2.js
+++
@@ -1,7 +0,0 @@
-export async function fetchData() {
-    // Здесь выполняется ваш запрос к внешнему API или базе данных
-    const response = await fetch('http://ec2-52-59-30-228.eu-central-1.compute.amazonaws.com/api/shop/');
-    const data = await response.json();
-    console.log(data)
-    return data.results;
-  }
